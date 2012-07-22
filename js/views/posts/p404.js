302ec9f933332f775d604b3bc3b2832a169d77c0
Diullei Gomes <diullei@gmail.com>
2012-07-22 18:22:49 -0300

    primeiro commit

---
+++ js/views/posts/p404.js
@@ -0,0 +1,15 @@
+﻿define([
+    'jQuery',
+    'Underscore',
+    'Backbone',
+    'text!views/templates/posts/404.html'
+], function($, _, Backbone, euTemplate){
+    var P404View = Backbone.View.extend({
+        el: $("#page"),
+        render: function(){
+            this.el.html(euTemplate);
+            $('#disqus_thread').hide();
+        }
+    });
+	return P404View;
+});
---
+++ js/views/posts/post.js
@@ -0,0 +1,36 @@
+define([
+    'jQuery',
+    'Underscore',
+    'Backbone',
+	'libs/showdown/showdown',
+	'libs/date/pretty',
+	'libs/highlight.min',
+    'text!views/templates/posts/post.html',
+    'text!views/templates/posts/tags.html'
+], function($, _, Backbone, Showdown, prettyDate, hljs, postTemplate, tagsTemplate){
+
+    var postView = Backbone.View.extend({
+        el: $("#page"),
+        constructor: function(model){
+            this.model = model;
+        },
+        render: function(){		
+            var model = (this.toJSON) ? this.model.toJSON() : this.model;
+
+			model.linkTitle = model.title
+			while (model.linkTitle.indexOf(' ') != -1) { model.linkTitle = model.linkTitle.replace(' ', '-'); }
+			
+            var data = {
+                    tags: model.tags,
+                    model: model,
+                    _: _ 
+                  };
+
+            model.content = new Showdown.converter().makeHtml(model.content);
+            this.el.html(_.template(postTemplate)(data));
+			$('pre code', this.el).each(function(i, e) {hljs.highlightBlock(e)});
+            $('#disqus_thread').show();
+        }
+    });
+    return postView;
+});
