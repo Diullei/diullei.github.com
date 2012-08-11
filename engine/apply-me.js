d37b1593769dd7a06cbc719eca3003b56f058c96
Rodrigo <rodrigovidal777@gmail.com>
2012-08-11 04:14:40 -0300

    Post template and me.js real data

---
+++ engine/apply-me.js
@@ -0,0 +1,26 @@
+var config = require('./me.js').cfg,
+	_ = require('./js/libs/underscore/underscore.js'),
+	fs = require('fs');
+
+_.templateSettings.evaluate = /\{\{([\s\S]+?)\}\}/g;
+_.templateSettings.interpolate = /\{\{=([\s\S]+?)\}\}/g;
+_.templateSettings.escape = /\{\{-([\s\S]+?)\}\}/g;
+
+var index_html = fs.readFileSync('index-tmpl.html', 'ascii');
+var post_template = fs.readFileSync('post-tmpl.html', 'ascii');
+
+var out_index_html = _.template(index_html)(config);
+fs.writeFile('../index.html', out_index_html.toString('utf8'), function(err) {
+    if(err) {
+        console.log(err);
+    }
+}); 
+
+var out_post_template = _.template(post_template)(config);
+fs.writeFile('js/views/templates/posts/post.html', out_post_template.toString('utf8'), function(err) {
+    if(err) {
+        console.log(err);
+    }
+}); 
+
+console.log("finish!");
---
+++ engine/me.js
@@ -0,0 +1,16 @@
+exports.cfg = {
+	"name" : "Diullei Gomes",
+	"github" : "https://www.github.com/Diullei",
+	"disqus" : "diulleigomes",
+	"twitter" : "http//www.twitter.com/diullei",
+	"twitterName" : "@diullei",
+	"linkedin" : "http://br.linkedin.com/in/diullei",
+	"email" : "diullei@gmail.com",
+	"gravatar" : "https://secure.gravatar.com/avatar/5e93890c5b54d18dd454f14789c2b225",
+	"blogroll" : [
+		{url: "http://rodrigovidal.net/", name: "Rodrigo Vidal"},
+		{url: "http://dotnetarchitects.net/", name: ".NET Architects"},
+        {url: "http://elemarjr.net/", name: "Elemar DEV"},
+        {url: "http://backbonetutorials.com/", name: "Backbone Tutorials"}
+	]
+};
