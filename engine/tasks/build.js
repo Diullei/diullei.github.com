86ebcd03b414b65cb8c54bd94b2a6793e13c6769
Diullei Gomes <diullei@gmail.com>
2012-08-12 21:04:23 -0300

    create install task

--- engine/tasks/build.js
+++ engine/tasks/build.js
@@ -1,13 +1,22 @@
+var fs = require('fs');
 var program = require('./../js/libs/commander');
 
-program
-  .version('0.0.1')
-  .option('-c, --compile', 'Compile blog')
-  .option('-a, --config', 'Apply user custom configs')
-  .option('-u, --update', 'Update BlogHub engine')
-  .parse(process.argv);
-  
-if(program.compile) {
+var _cmd = {};
+
+function directoryExists(path) {
+	try {
+		stats = fs.lstatSync(path);
+		if (stats.isDirectory()) {
+			return true;
+		}
+	}
+	catch (e) {
+		return false;
+	}
+	return false;
+}
+
+_cmd.compile = function(){
 	console.log('Compiling resources...\n');
 	
 	try{
@@ -17,9 +26,10 @@ if(program.compile) {
 	catch(err){
 		console.log('Error: ' + err.message);
 	}
-} else if(program.config) {
+};
+
+_cmd.config = function(){
 	console.log('Applying ser configurations...');
-	
 	try{
 		require('./apply-me').applyConfig();
 		console.log('\nSuccessful');
@@ -27,4 +37,45 @@ if(program.compile) {
 	catch(err){
 		console.log('Error: ' + err.message);
 	}
+};
+
+_cmd.install = function(){
+	console.log('Installing blog...');
+	
+	if(!directoryExists('../blog')) {
+		console.log('[install] create directory: /blog');
+		fs.mkdirSync('../blog');
+	}
+	if(!directoryExists('../blog/img')) {
+		console.log('[install] create directory: /blog/img');
+		fs.mkdirSync('../blog/img');
+	}
+	if(!directoryExists('../blog/compiled-sources')) {
+		console.log('[install] create directory: /blog/compiled-sources');
+		fs.mkdirSync('../blog/compiled-sources');
+	}
+	if(!directoryExists('../blog/posts')) {
+		console.log('[install] create directory: /blog/posts');
+		fs.mkdirSync('../blog/posts');
+	}
+	
+	fs.writeFileSync('../start_server.cmd', 'node engine\\server.js 100 .');	
+	console.log('[install] create file: start_server.cmd');
+}
+
+program
+  .version('0.0.1')
+  .option('-c, --compile', 'Compile blog')
+  .option('-a, --config', 'Apply user custom configs')
+  .option('-u, --update', 'Update BlogHub engine')
+  .option('-i, --install', 'Install blog')
+  .parse(process.argv);
+
+if(program.compile) {
+	_cmd.compile();
+} else if(program.config) {
+	_cmd.config();
+} else if (program.install){
+	_cmd.install();
+	_cmd.config();
 }
